8a1a9716c1594e0c23c52596d9352ae39859441f
Ian Young <ian.greenleaf@gmail.com>
2009-11-03 23:20:05 -0600

    Adding a bunch more tests

--- tests/RenameMethodTest.php
+++ tests/RenameMethodTest.php
@@ -88,6 +88,106 @@ EOL;
         $this->renameAndCompare($orig, $expected);
     }
 
+    public function testOnlyRenameIfInScope() {
+        $orig = <<<EOL
+<?php
+\$f = new NotFoo();
+function quark(\$param) {
+    \$f = new Foo();
+    return \$f->bar();
+}
+\$result = \$f->bar();
+EOL;
+        $expected = <<<EOL
+<?php
+\$f = new NotFoo();
+function quark(\$param) {
+    \$f = new Foo();
+    return \$f->baz();
+}
+\$result = \$f->bar();
+EOL;
+        $this->renameAndCompare($orig, $expected);
+    }
+
+    public function testRenameGlobalVariable() {
+        $orig = <<<EOL
+<?php
+\$f = new Foo();
+function quark(\$param) {
+    global \$f;
+    return \$f->bar();
+}
+EOL;
+        $expected = <<<EOL
+<?php
+\$f = new Foo();
+function quark(\$param) {
+    global \$f;
+    return \$f->baz();
+}
+EOL;
+        $this->renameAndCompare($orig, $expected);
+    }
+
+    public function testDontRenameNonGlobalVariable() {
+        $orig = <<<EOL
+<?php
+\$f = new Foo();
+function quark(\$param) {
+    return \$f->bar();
+}
+EOL;
+        $expected = <<<EOL
+<?php
+\$f = new Foo();
+function quark(\$param) {
+    return \$f->bar();
+}
+EOL;
+        $this->renameAndCompare($orig, $expected);
+    }
+
+    public function testScopeOrder() {
+        $orig = <<<EOL
+<?php
+function quark(\$param) {
+    \$a = new Foo();
+    \$b = new NotFoo();
+    \$c = new NotFoo();
+    \$d = new Foo();
+    function strangeQuark()
+        \$b = new Foo();
+        \$d = new NotFoo();
+        \$a->bar();
+        \$b->bar();
+        \$c->bar();
+        \$d->bar();
+    }
+    return \$f->bar();
+}
+EOL;
+        $expected = <<<EOL
+<?php
+function quark(\$param) {
+    \$a = new Foo();
+    \$b = new NotFoo();
+    \$c = new NotFoo();
+    \$d = new Foo();
+    function strangeQuark()
+        \$b = new Foo();
+        \$d = new NotFoo();
+        \$a->baz();
+        \$b->baz();
+        \$c->bar();
+        \$d->bar();
+    }
+    return \$f->bar();
+}
+EOL;
+        $this->renameAndCompare($orig, $expected);
+    }
+
     public function testRenameMethodStaticCall() {
         $orig = <<<EOL
 <?php
