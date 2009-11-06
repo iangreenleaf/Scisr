623642e2c722a0b295e0a3f8483779a4aebffefd
Ian Young <ian.greenleaf@gmail.com>
2009-11-05 20:36:45 -0600

    Changing how we track variable types to be more versatile and catch var->var assignments

--- Operations/TrackVariableTypes.php
+++ Operations/TrackVariableTypes.php
@@ -12,30 +12,39 @@ class Scisr_Operations_TrackVariableTypes implements PHP_CodeSniffer_Sniff
     public function register()
     {
         return array(
-            T_NEW,
+            T_EQUAL,
+            T_AND_EQUAL,
         );
     }
 
     public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
         $tokens = $phpcsFile->getTokens();
-        $prevPtr = $phpcsFile->findPrevious(array(T_WHITESPACE), $stackPtr - 1, null, true);
-        $prevToken = $tokens[$prevPtr];
-        $classPtr = $phpcsFile->findNext(T_STRING, $stackPtr);
-        $classToken = $tokens[$classPtr];
-        $className = $classToken['content'];
-        // See if we are assigning the class to a variable
-        if ($prevToken['code'] == T_EQUAL || $prevToken['code'] == T_AND_EQUAL) {
-            $varPtr = $phpcsFile->findPrevious(array(T_WHITESPACE), $prevPtr - 1, null, true);
-            $varToken = $tokens[$varPtr];
-            if ($varToken['code'] == T_VARIABLE) {
-                Scisr_VariableTypes::registerVariableType(
-                    $varToken['content'],
-                    $className,
-                    $phpcsFile->getFileName(),
-                    $varToken['conditions']
-                );
-            }
+        $varPtr = $phpcsFile->findPrevious(array(T_WHITESPACE), $stackPtr - 1, null, true);
+        $varToken = $tokens[$varPtr];
+
+        // If we are not assigning the class to a variable, quit
+        if ($varToken['code'] != T_VARIABLE) {
+            return;
+        }
+
+        $nextPtr = $phpcsFile->findNext(array(T_WHITESPACE), $stackPtr + 1, null, true);
+        $nextToken = $tokens[$nextPtr];
+        if ($nextToken['code'] == T_NEW) {
+            $classPtr = $phpcsFile->findNext(T_STRING, $nextPtr);
+            $classToken = $tokens[$classPtr];
+            $className = $classToken['content'];
+        } else if ($nextToken['code'] == T_VARIABLE) {
+            $className = Scisr_VariableTypes::getVariableType($nextToken['content'], $phpcsFile->getFileName(), $nextToken['conditions']);
+        }
+
+        if (isset($className) && $className !== null) {
+            Scisr_VariableTypes::registerVariableType(
+                $varToken['content'],
+                $className,
+                $phpcsFile->getFileName(),
+                $varToken['conditions']
+            );
         }
     }
 }
