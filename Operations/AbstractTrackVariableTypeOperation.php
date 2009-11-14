iangreenleaf/Scisr

commit 5855b089247cecbe258aa18271cec7212307311e

Abstracting out more shared functionality

Operations/AbstractTrackVariableTypeOperation.php
@@ -1,0 +1,49 @@
+<?php
+
+/**
+ * Helps with tracking variable types specified in PHPDoc tags
+ */
+abstract class Scisr_Operations_AbstractTrackVariableTypeOperation
+    extends Scisr_Operations_AbstractVariableTypeOperation
+{
+
+    protected $_phpcsFile;
+    protected $_lastParsed = 0;
+
+    public function register()
+    {
+        return array(
+            T_COMMENT,
+            T_DOC_COMMENT,
+        );
+    }
+
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        // If we've already parsed this comment, pass
+        if ($phpcsFile == $this->_phpcsFile && $stackPtr < $this->_lastParsed) {
+            return;
+        }
+
+        $this->_phpcsFile = $phpcsFile;
+        $tokens = $phpcsFile->getTokens();
+        // Find the end of the comment block
+        $endPtr = $phpcsFile->findNext($tokens[$stackPtr]['code'], $stackPtr + 1, null, true);
+        $this->_lastParsed = $endPtr;
+        // Get the whole comment text
+        $comment = $phpcsFile->getTokensAsString($stackPtr, $endPtr - $stackPtr + 1);
+        $this->parser = new Scisr_CommentParser_ChangeTagValues($comment, $phpcsFile);
+        $this->parser->parse();
+
+        $elements = $this->parser->getTagElements();
+
+        foreach ($elements as $tagName => $tagArray) {
+            $method = array($this, 'process' . ucfirst($tagName));
+            if (is_callable($method)) {
+                foreach ($tagArray as $i => $tag) {
+                    call_user_func($method, $tag, $stackPtr, $phpcsFile);
+                }
+            }
+        }
+    }
+}

Operations/ChangeClassNameComments.php
@@ -3,56 +3,18 @@
 /**
  * An operation to change the name of a class in PHPDoc tags
  */
-class Scisr_Operations_ChangeClassNameComments implements PHP_CodeSniffer_Sniff
+class Scisr_Operations_ChangeClassNameComments
+    extends Scisr_Operations_AbstractTrackVariableTypeOperation
+    implements PHP_CodeSniffer_Sniff
 {
 
     public $oldName;
     public $newName;
-    protected $_phpcsFile;
-    protected $_lastParsed = 0;
 
     public function __construct($oldName, $newName)
     {
         $this->oldName = $oldName;
         $this->newName = $newName;
-    }
-
-    public function register()
-    {
-        return array(
-            T_COMMENT,
-            T_DOC_COMMENT,
-        );
-    }
-
-    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
-    {
-        // If we've already parsed this comment, pass
-        if ($phpcsFile == $this->_phpcsFile && $stackPtr < $this->_lastParsed) {
-            return;
-        }
-
-        $this->_phpcsFile = $phpcsFile;
-        $tokens = $phpcsFile->getTokens();
-        // Find the end of the comment block
-        $endPtr = $phpcsFile->findNext($tokens[$stackPtr]['code'], $stackPtr + 1, null, true);
-        $this->_lastParsed = $endPtr;
-        // Get the whole comment text
-        $comment = $phpcsFile->getTokensAsString($stackPtr, $endPtr - $stackPtr + 1);
-        $this->parser = new Scisr_CommentParser_ChangeTagValues($comment, $phpcsFile);
-        $this->parser->parse();
-
-        $elements = $this->parser->getTagElements();
-        $columns = $this->parser->getTagElementColumns();
-
-        foreach ($elements as $tagName => $tagArray) {
-            $method = array($this, 'process' . ucfirst($tagName));
-            if (is_callable($method)) {
-                foreach ($tagArray as $i => $tag) {
-                    call_user_func($method, $tag, $tokens[$stackPtr], $columns[$tagName][$i]);
-                }
-            }
-        }
     }
 
     protected function processVar($var, $commentToken, $columns)

Operations/TrackCommentVariableTypes.php
@@ -4,49 +4,9 @@
  * Tracks variable types specified in PHPDoc tags
  */
 class Scisr_Operations_TrackCommentVariableTypes
-    extends Scisr_Operations_AbstractVariableTypeOperation
+    extends Scisr_Operations_AbstractTrackVariableTypeOperation
     implements PHP_CodeSniffer_Sniff
 {
-
-    protected $_phpcsFile;
-    protected $_lastParsed = 0;
-
-    public function register()
-    {
-        return array(
-            T_COMMENT,
-            T_DOC_COMMENT,
-        );
-    }
-
-    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
-    {
-        // If we've already parsed this comment, pass
-        if ($phpcsFile == $this->_phpcsFile && $stackPtr < $this->_lastParsed) {
-            return;
-        }
-
-        $this->_phpcsFile = $phpcsFile;
-        $tokens = $phpcsFile->getTokens();
-        // Find the end of the comment block
-        $endPtr = $phpcsFile->findNext($tokens[$stackPtr]['code'], $stackPtr + 1, null, true);
-        $this->_lastParsed = $endPtr;
-        // Get the whole comment text
-        $comment = $phpcsFile->getTokensAsString($stackPtr, $endPtr - $stackPtr + 1);
-        $this->parser = new Scisr_CommentParser_ChangeTagValues($comment, $phpcsFile);
-        $this->parser->parse();
-
-        $elements = $this->parser->getTagElements();
-
-        foreach ($elements as $tagName => $tagArray) {
-            $method = array($this, 'process' . ucfirst($tagName));
-            if (is_callable($method)) {
-                foreach ($tagArray as $i => $tag) {
-                    call_user_func($method, $tag, $stackPtr, $phpcsFile);
-                }
-            }
-        }
-    }
 
     protected function processVar($var, $commentPtr, $phpcsFile)
     {

commit 8d612cf6d0c8c644ffa283a0a357fed40d0ec808

Fixing regression caused by 5855b089247cecbe258aa18271cec7212307311e

Operations/AbstractTrackVariableTypeOperation.php
@@ -36,12 +36,13 @@ abstract class Scisr_Operations_AbstractTrackVariableTypeOperation
         $this->parser->parse();
 
         $elements = $this->parser->getTagElements();
+        $columns = $this->parser->getTagElementColumns();
 
         foreach ($elements as $tagName => $tagArray) {
             $method = array($this, 'process' . ucfirst($tagName));
             if (is_callable($method)) {
                 foreach ($tagArray as $i => $tag) {
-                    call_user_func($method, $tag, $stackPtr, $phpcsFile);
+                    call_user_func($method, $tag, $stackPtr, $phpcsFile, $columns[$tagName][$i]);
                 }
             }
         }

Operations/ChangeClassNameComments.php
@@ -17,24 +17,26 @@ class Scisr_Operations_ChangeClassNameComments
         $this->newName = $newName;
     }
 
-    protected function processVar($var, $commentToken, $columns)
+    protected function processVar($var, $commentPtr, $phpcsFile, $columns)
     {
-        $this->findWordChanges($var, array('content'), $commentToken, $columns);
+        $this->findWordChanges($var, array('content'), $commentPtr, $phpcsFile, $columns);
     }
 
-    protected function processParam($param, $commentToken, $columns)
+    protected function processParam($param, $commentPtr, $phpcsFile, $columns)
     {
-        $this->findWordChanges($param, array('type'), $commentToken, $columns);
+        $this->findWordChanges($param, array('type'), $commentPtr, $phpcsFile, $columns);
     }
 
-    protected function processReturn($param, $commentToken, $columns)
+    protected function processReturn($param, $commentPtr, $phpcsFile, $columns)
     {
-        $this->findWordChanges($param, array('value'), $commentToken, $columns);
+        $this->findWordChanges($param, array('value'), $commentPtr, $phpcsFile, $columns);
     }
 
-    protected function findWordChanges($docElement, $wordTypes, $commentToken, $columns)
+    protected function findWordChanges($docElement, $wordTypes, $commentPtr, $phpcsFile, $columns)
     {
         $subElements = $docElement->getSubElementValues();
+        $tokens = $phpcsFile->getTokens();
+        $commentToken = $tokens[$commentPtr];
         $line = $commentToken['line'] + $docElement->getLine();
         $i = 0;
         foreach ($subElements as $name => $value) {